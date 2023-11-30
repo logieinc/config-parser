92ed5e91115b3be97bd1154472fa3358dba2e451
Claudio Romano <claudioandresromano@gmail.com>
2023-11-30 18:28:11 -0300

    pruebas

---
+++ ppt/printdatos.py
@@ -0,0 +1,21 @@
+# Impresion setores
+
+import configparser
+
+
+def print_config(config):
+    # Imprimir todas las secciones y opciones
+    print()
+    for section in config.sections():
+        print(f"[{section}]")
+        for option in config.options(section):
+            value = config.get(section, option)
+            print(f"{option} = {value} -> {type(value)}")
+        print()  # Agregar una línea en blanco entre secciones
+
+
+configiso = configparser.ConfigParser()
+
+configiso.read("configiso.ini")
+
+print_config(configiso)
