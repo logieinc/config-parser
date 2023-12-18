be1f83f7a5ea36ffc9add84c11ec95194cb17994
Claudio Romano <claudioandresromano@gmail.com>
2023-12-18 19:03:16 -0300

    Revisión con Fabian

---
+++ pruebas-Claudio/ejemplos dicttype/dict_type_02.py
@@ -0,0 +1,22 @@
+from configparser import ConfigParser
+
+class CaseInsensitiveDict(dict):
+    def __setitem__(self, key, value):
+        super(CaseInsensitiveDict, self).__setitem__(key.lower(), value)
+
+    def __getitem__(self, key):
+        return super(CaseInsensitiveDict, self).__getitem__(key.lower())
+
+    def __contains__(self, key):
+        return super(CaseInsensitiveDict, self).__contains__(key.lower())
+
+# Crear un objeto ConfigParser y especificar el tipo de dict personalizado
+config = ConfigParser(interpolation=None, dict_type=CaseInsensitiveDict)
+
+# Agregar opciones de configuración con claves insensibles a mayúsculas y minúsculas
+config['Section1'] = {'Option1': 'Value1', 'Option2': 'Value2'}
+config['Section2'] = {'Option3': 'Value3', 'Option4': 'Value4'}
+
+# Acceder a las opciones de configuración con claves insensibles a mayúsculas y minúsculas
+print(config['section1']['option1'])  # Salida: Value1
+print(config['SECTION2']['OPTION3'])  # Salida: Value3
